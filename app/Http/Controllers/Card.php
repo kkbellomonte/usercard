71e0522102b95ff84cbc586888d5c1f441e94ffd
Karn Kallio <kkallio@sid.eka.machine.skami.org>
2020-05-25 11:20:07 -0400

    Validate the user id when displaying their card.

--- app/Http/Controllers/Card.php
+++ app/Http/Controllers/Card.php
@@ -5,8 +5,27 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
+use App\Person;
+
 class Card extends Controller
 {
+	/**
+	 * Look up a person by database id.
+	 *
+	 * If the database query fails due to a non-integer id or there
+	 * is no user with a given id, returns null, otherwise the corresponding
+	 * Person object.
+	 *
+	 */
+	 private static function lookupPerson($id) {
+	 	 try {
+	 	 	 return Person::findOrFail($id);
+	 	 }
+	 	 catch (\Exception $_) {
+	 	 	return null;	 
+	 	 }
+	 }
+	 
     /**
      * Display a card for a person.
      *
@@ -14,7 +33,12 @@ class Card extends Controller
      * @return View
      */
      public function display($id) {
-     	 $person = \App\Person::findOrFail((int) $id);
+     	 $person = self::lookupPerson($id);
+     	 
+     	 if ( is_null($person) )
+     	 {
+     	 	return response('No such user (2)', 404);	 
+     	 }
      	 
      	 return view('card')->with('person', $person);	 
      }
