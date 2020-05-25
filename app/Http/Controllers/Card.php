028673012775145fd6b68db15396a51e094363d3
Karn Kallio <kkallio@sid.eka.machine.skami.org>
2020-05-25 13:05:20 -0400

    Add validations and append the comments.

--- app/Http/Controllers/Card.php
+++ app/Http/Controllers/Card.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Validator;
 
 use App\Person;
@@ -25,7 +26,43 @@ class Card extends Controller
 	 	 	return null;	 
 	 	 }
 	 }
+
+	 /**
+	  * Determines whether a non-empty request body IS NOT proper JSON.
+	  */
+	  private static function badJSONRequest(Request $request) {
+	  	  if ( empty($request->getContent()) ) { return false; }
+	  	  
+	  	  json_decode($request->getContent());
+	  	  
+	  	  return JSON_ERROR_NONE !== json_last_error();
+	  }
 	 
+	  /**
+	   * Checks the authorization secret.
+	   */
+	   private static function passwordOk($password) {
+	   		return strtoupper($password) ===  strtoupper(config('parameters.secrets.password'));   
+	   }
+	  
+	/**
+	 * Carries out updating the comments for a person.
+	 */
+	 private static function doAppendComments($id, $comments)
+	 {
+     	 $person = self::lookupPerson($id);
+     	 
+     	 if ( is_null($person) )
+     	 {
+     	 	return response('No such user (1)', 404);	 
+     	 }	 	 
+	 	 
+     	 $person->comments .= PHP_EOL . $comments;
+     	 $person->save();
+     	 
+	 	 return response('OK', 200);
+	 }
+	   
     /**
      * Display a card for a person.
      *
@@ -49,6 +86,39 @@ class Card extends Controller
       * Requires a secret (password/token) for authorization.
       */
       public function appendComments(Request $request) {
-      	  return response('OK');
+      	  
+      	  // Guard on improper JSON content.
+      	  if ( self::badJSONRequest($request) ) {
+      	  	return response('Invalid POST JSON', 422);	  
+      	  }
+      	  
+      	  $input = empty($request->getContent()) ? $request->all() : $request->json()->all();
+      	  
+      	  // Validate that all the required fields are present.
+      	  $validator = Validator::make(
+      	  	  $input,
+      	  	  array_fill_keys(['password', 'id', 'comments'], ['required']),
+      	  	  ['required' => 'Missing key/value for ":attribute"']
+      	  );
+
+      	  if ( $validator->fails() )
+      	  {
+      	  	  $message = Arr::first($validator->errors()->all());
+
+      	  	  return response($message, 422);   
+      	  }
+      	  
+      	  // Check the authorization secret is correct.
+      	  if ( ! self::passwordOk($input['password']) )
+      	  {
+      	  	return response('Invalid password', 401);  
+      	  }
+      	  
+      	  // Guard that the identifier is numeric.
+      	  if ( ! is_numeric($input['id']) ) {
+      	  	  return response('Invalid id', 422);
+      	  }
+      	  
+      	  return self::doAppendComments($input['id'], $input['comments']);
       }
 }
---
+++ config/parameters.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+	'secrets' => [
+		'password' => '720DF6C2482218518FA20FDC52D4DED7ECC043AB'
+	]
+];
