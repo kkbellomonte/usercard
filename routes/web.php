d40de4b975065421632d16665f59d7eabf8ae9d4
Karn Kallio <kkallio@sid.eka.machine.skami.org>
2020-05-25 16:40:15 -0400

    Add some notes to the routes.

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,27 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+/**
+ * Displays the card for a user selected by database identifier.
+ */
 Route::get('/user/{id}', 'Card@display');
 
+/**
+ * Adds a new line to the comments on a person's card.
+ *
+ * The post may either have a HTML form Content-Type or be raw JSON.
+ * In either case, the following parameters must be present.
+ *
+ * @param int id
+ * @param string password
+ * @param string comments
+ */
 Route::post('/', 'Card@appendComments');
 
+/**
+ * We respond with a no user message if we don't recognize the route,
+ * as the legacy controller does.
+ */
 Route::fallback(function () {
 	return response('No such user (3)', 404);
 });
